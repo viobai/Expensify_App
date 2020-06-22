9febe19a15e1ce442c24ae6ecf73880831d6e050
viola <violabaity@gmail.com>
2020-06-22 12:16:58 -0600

    Production mode and source-map updated

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,57 @@
 // entry point of application -> final output loc
 // config change --watch does not work
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-module.exports = {
-    entry: './src/app.js',
-    output:{
-        path: path.join(__dirname,'public'),
-        filename:'bundle.js'
-    },
-    // sets rules (conditions, results and nested Rules)
-    // runs babel everytime seeing a js file
-    module: {
-        rules: [{
-            loader: 'babel-loader',
-            test: /\.js$/,
-            exclude: /node_modules/
-        }, {
-            test: /\.s?css$/,
-            use: [
-                'style-loader',
-                'css-loader',
-                'sass-loader'
-            ]
-         }]
-    },
-    devtool: 'cheap-module-eval-source-map',
-    // devserver makes bundle.js in live server, no physical one, makes running faster
-    devServer: {
-        contentBase: path.join(__dirname, 'public'),
-        historyApiFallback: true
+module.exports = (env) => {
+    const isProduction = env === 'production';
+    const CSSExtract = new ExtractTextPlugin('styles.css');
+    console.log('env', env);
+    return {
+        entry: './src/app.js',
+        output: {
+            path: path.join(__dirname, 'public'),
+            filename: 'bundle.js'
+        },
+        // sets rules (conditions, results and nested Rules)
+        // runs babel everytime seeing a js file
+        module: {
+            rules: [{
+                loader: 'babel-loader',
+                test: /\.js$/,
+                exclude: /node_modules/
+            }, {
+                test: /\.s?css$/,
+                use: CSSExtract.extract({
+                    use: [
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        },
+                        {
+                            loader: 'sass-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        },
+                    ]
+                })
+            }]
+        },
+        plugins: [
+            CSSExtract
+        ],
+        devtool: isProduction ? 'source-map': 'inline-source-map',
+        // devserver makes bundle.js in live server, no physical one, makes running faster
+        devServer: {
+            contentBase: path.join(__dirname, 'public'),
+            historyApiFallback: true
+        }
     }
-};
+}
+
+
 
 
