4551db5405c8d3858146a9867c1856d1091bec8b
viola <violabaity@gmail.com>
2020-06-26 14:02:40 -0600

    fixed firebase removing bug

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -36,7 +36,8 @@ export const removeExpense = ({ id } = {}) => ({
 export const startRemoveExpense = ({ id } = {}) => {
     return (dispatch, getState) => {
         const uid = getState().auth.uid;
-        return database.ref('users/' + uid + '/expenses' + id).remove().then(() => {
+        console.log(id);
+        return database.ref('users/' + uid + '/expenses/' + id).remove().then(() => {
             dispatch(removeExpense({ id }));
         });
     };
